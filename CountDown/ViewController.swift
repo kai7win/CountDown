e8d242312042bda9f9c8e26d0a1a1f87407261ca
KaiChi <34007164+kai7win@users.noreply.github.com>
2023-01-21 21:51:16 +0800

    Initial Commit

---
+++ CountDown/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CountDown
+//
+//  Created by Kai Chi Tsao on 2023/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
